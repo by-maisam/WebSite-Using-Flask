3b2cc71361d979e941aae9924499a295029fa92b
by-maisam <maisamraz473@gmail.com>
2026-06-10 01:55:59 -0700

    Added Templates and Rendered

--- auth.py
+++ auth.py
@@ -1,12 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint ,render_template
 
 auth = Blueprint('auth',__name__)
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html")
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 @auth.route('/signup')
 def signup():
-    return "<p>signup</p>"
+    return render_template("signup.html")
